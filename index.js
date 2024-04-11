2d11ab428ffb1b048edce1a77e9b049637069716
setar92 <tarasiuks2303@gmail.com>
2024-04-11 21:16:53 +0300

    added express

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// index.js
+
+const express = require('express');
 const cron = require('node-cron');
 const mainlLogick = require('./helpers/check_new_parcels');
 require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 3000;
 
 const period = process.env.PERIOD;
 // Scheduling a function call every period minutes
@@ -16,3 +21,13 @@ cron.schedule(`*/${period} * * * *`, async () => {
 }, {
     timezone: 'Europe/Riga' // Setting the time zone Latvia (Riga)
 });
+
+app.get('/', (req, res) => {
+    res.send('The server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+module.exports = app;
