da74cc8d4a1f277e0e0820868630bab80dbbc42c
setar92 <tarasiuks2303@gmail.com>
2024-04-11 18:49:20 +0300

    created express server

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+// index.js
+
+const express = require('express');
 const cron = require('node-cron');
 const mainlLogick = require('./helpers/check_new_parcels');
-require('dotenv').config()
+require('dotenv').config();
 
+const app = express();
+const port = process.env.PORT || 3000;
 
 const period = process.env.PERIOD;
 // Scheduling a function call every period minutes
-const app = cron.schedule(`*/${period} * * * *`, async () => {
+cron.schedule(`*/${period} * * * *`, async () => {
     // Time check for the period from 22:00 to 08:00
     const now = new Date();
     const hour = now.getHours();
@@ -17,6 +22,12 @@ const app = cron.schedule(`*/${period} * * * *`, async () => {
     timezone: 'Europe/Riga' // Setting the time zone Latvia (Riga)
 });
 
-console.log('The server is running');
+app.get('/', (req, res) => {
+    res.send('The server is running');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
