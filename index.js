a771229743eb5d3074f75d8996506a54491191c4
setar92 <tarasiuks2303@gmail.com>
2024-04-11 19:59:22 +0300

    deleted express

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-// index.js
-
-const express = require('express');
 const cron = require('node-cron');
 const mainlLogick = require('./helpers/check_new_parcels');
 require('dotenv').config();
 
-const app = express();
-const port = process.env.PORT || 3000;
 
 const period = process.env.PERIOD;
 // Scheduling a function call every period minutes
@@ -21,13 +16,3 @@ cron.schedule(`*/${period} * * * *`, async () => {
 }, {
     timezone: 'Europe/Riga' // Setting the time zone Latvia (Riga)
 });
-
-app.get('/', (req, res) => {
-    res.send('The server is running');
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
-
-module.exports = app;
