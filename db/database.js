ce08894ee66c96cb781b77416513ef7a9b8f57c4
setar92 <tarasiuks2303@gmail.com>
2024-04-25 19:24:42 +0300

    new data base

--- db/database.js
+++ db/database.js
@@ -1,12 +1,27 @@
-const { Pool } = require('pg')
-require('dotenv').config()
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
-})
+const { Pool } = require('pg');
+require('dotenv').config();
+const fs = require('fs');
+const path = require('path');
+
+const caCert = fs.readFileSync(path.resolve(__dirname, 'ca.pem')).toString();
+
+const config = {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE,
+    ssl: {
+        rejectUnauthorized: true,
+        // Вказуємо сертифікат прямо в коді
+        ca: caCert,
+}};
+
+const pool = new Pool(config);
 
 pool.connect((err) => {
-    if (err) throw err
-    console.log("Connect to PostgreSQL successfully!")
-})
+    if (err) throw err;
+    console.log("Connected to PostgreSQL successfully!");
+});
 
-module.exports = pool
+module.exports = pool;
