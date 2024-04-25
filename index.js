6452a9d2a1aa2ead7b291c46c95ab6540596364c
setar92 <tarasiuks2303@gmail.com>
2024-04-25 19:28:36 +0300

    deleted time checker

--- index.js
+++ index.js
@@ -11,13 +11,7 @@ const port = process.env.PORT || 3000;
 const period = process.env.PERIOD;
 // Scheduling a function call every period minutes
 cron.schedule(`*/${period} * * * *`, async () => {
-    // Time check for the period from 22:00 to 08:00
-    const now = new Date();
-    const hour = now.getHours();
-
-    if (hour >= 8 && hour < 22) {
         await mainlLogick();
-    }
 }, {
     timezone: 'Europe/Riga' // Setting the time zone Latvia (Riga)
 });
